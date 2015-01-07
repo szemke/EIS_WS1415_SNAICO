3d523034dc82b80807849d98e8369bf9b01cccc1
Simon Zemke <simon.zemke@gmail.com>
2015-01-07 01:24:09 +0100

    Start SNAICO

---
+++ SNAICO/app/src/main/java/eis1415/rebecca/simon/snaico/JSONParser.java
@@ -0,0 +1,110 @@
+package eis1415.rebecca.simon.snaico;
+
+/**
+ * Created by Simon on 09.11.2014.
+ */
+
+import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+public class JSONParser {
+
+    static InputStream is = null;
+    static JSONArray jObj = null;
+    static String json = "";
+
+    // constructor
+    public JSONParser() {
+
+    }
+
+    // function get json from url
+    // by making HTTP POST or GET method
+    public JSONArray makeHttpRequest(String url, String method, List params) {
+
+        // Making HTTP request
+        try {
+
+            // check for request method
+            if(method == "POST"){
+                // request method is POST
+                // defaultHttpClient
+                DefaultHttpClient httpClient = new DefaultHttpClient();
+                HttpPost httpPost = new HttpPost(url);
+                try {
+                    httpPost.setEntity(new UrlEncodedFormEntity(params));
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                    Log.d("jsonpost", "setEntity FAIL");
+                }
+                HttpResponse httpResponse = httpClient.execute(httpPost);
+                HttpEntity httpEntity = httpResponse.getEntity();
+                is = httpEntity.getContent();
+                StatusLine statusLine = httpResponse.getStatusLine();
+                int statusCode = statusLine.getStatusCode();
+                Log.d("jsonPost", "status code: "+statusCode);
+
+            }else if(method == "GET"){
+                // request method is GET
+                DefaultHttpClient httpClient = new DefaultHttpClient();
+                String paramString = URLEncodedUtils.format(params, "utf-8");
+                url += "?" + paramString;
+                HttpGet httpGet = new HttpGet(url);
+
+                HttpResponse httpResponse = httpClient.execute(httpGet);
+                HttpEntity httpEntity = httpResponse.getEntity();
+                is = httpEntity.getContent();
+            }
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    is, "iso-8859-1"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+            json = sb.toString();
+        } catch (Exception e) {
+            Log.e("Buffer Error", "Error converting result " + e.toString());
+        }
+
+        // try parse the string to a JSON object
+        try {
+            jObj = new JSONArray(json);
+        } catch (JSONException e) {
+            Log.e("JSON Parser", "Error parsing data " + e.toString());
+        }
+
+        // return JSON String
+        return jObj;
+
+    }
+}
